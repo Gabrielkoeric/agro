ad4e055f32a36653f9baf25089e3030b9e714101
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2024-08-13 01:47:55 +0000

    inicio do projeto

---
+++ database/migrations/2024_08_12_222515_create_home_table.php
@@ -0,0 +1,130 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home', function (Blueprint $table) {
+            $table->id('id_home');
+            $table->string('nome');
+            $table->string('nome_tela');
+            $table->string('imagem_tela');
+            $table->boolean('permite_home');
+            $table->timestamps();
+        });
+
+        $dadosPadraoHome = [
+            [
+                'nome' => 'Servidores Fisicos',
+                'nome_tela' => 'server',
+                'imagem_tela' => 'storage/home/servidor.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'CPU',
+                'nome_tela' => 'cpu',
+                'imagem_tela' => 'storage/home/cpu.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Memória',
+                'nome_tela' => 'memoria',
+                'imagem_tela' => 'storage/home/memoria.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'HD',
+                'nome_tela' => 'hd',
+                'imagem_tela' => 'storage/home/hd.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'IP Publico',
+                'nome_tela' => 'ip_publico',
+                'imagem_tela' => 'storage/home/ip_publico.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Busca de IPs',
+                'nome_tela' => 'ip',
+                'imagem_tela' => 'storage/home/ip.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'SSH',
+                'nome_tela' => 'ssh',
+                'imagem_tela' => 'n/t',
+                'permite_home' => 0,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Usuarios',
+                'nome_tela' => 'usuario',
+                'imagem_tela' => 'storage/home/usuarios.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Perfis de Usuarios',
+                'nome_tela' => 'perfis_usuarios',
+                'imagem_tela' => 'storage/home/perfis_usuarios.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Logs de Acesso',
+                'nome_tela' => 'access_logs',
+                'imagem_tela' => 'storage/home/log_access.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+            [
+                'nome' => 'Scripts',
+                'nome_tela' => 'script',
+                'imagem_tela' => 'storage/home/terraform.png',
+                'permite_home' => 1,
+                'created_at' => null,
+                'updated_at' => null,
+            ],
+        ];
+
+        DB::table('home')->insert($dadosPadraoHome);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\HomeController;
+use App\Http\Middleware\Autenticador;
+use App\Http\Middleware\ControleAcesso;
+use App\Http\Controllers\SocialiteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//home
+Route::get('/home', [HomeController::class, 'index'])->name('home.index')->middleware(Autenticador::class);
+//usuarios
+Route::resource('/usuario', UsuarioController::class)->middleware(Autenticador::class)->middleware(ControleAcesso::class);
+
+//login google
+//Route::get('login/google', "SocialiteController@redirectToProvider");
+//Route::get('login/google/callback', 'SocialiteController@handleProviderCalback');
+Route::get('login/google', [SocialiteController::class, 'redirectToProvider'])->name('login');
+Route::get('login/google/callback', [SocialiteController::class, 'hendProviderCallback']);
+Route::get('login/logout', [SocialiteController::class, 'destroy'])->name('logout');
+Route::get('/forbidden', function () {return view('forbidden.index');});
